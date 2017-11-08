aaf3c613c3b7da4f2baf4f7149b9935a367a4706
Tjccs <tiago.jccs@gmail.com>
2017-11-08 14:51:51 +0000

    Add files via upload

--- pzip.py
+++ pzip.py
@@ -2,55 +2,84 @@
 
 import os
 import sys
-from multiprocessing import Process
+from multiprocessing import Process, Value
 import zipfile
 import argparse
 
+count = Value('i',0)
 def pyzip(args):
-    
-    
-    print(args)
-  
+	global count
+	for files in args:
+		with zipfile.ZipFile(files+'.zip', mode='w') as zf:
+			zf.write(files)
+			count.value += 1
+			
+def pyunzip(args):
+	global count
+	for files in args:
+		with zipfile.ZipFile(files) as zf:
+			zf.extractall()
+			count.value += 1
 
-def pyunzip(args2):
-    
-    print(args2)
 
-    
+
+
+
 if __name__ == '__main__':
-    #argumentos a utilizar
-    parser = argparse.ArgumentParser(prog='pzip', description='Zip or unzip files')
-    parser.add_argument('-c', action='store_const', dest='pyzip', const=True)
-    parser.add_argument('-d', action='store_const', dest='pyunzip', const=True)
-    parser.add_argument('-p', metavar='n', nargs='?', action='append', dest='process', const=True)
-    args = parser.parse_args()
-    
-    #lista dos processos
-    processes = []
-    
-    #verificação de argumentos
-    if args.pyzip == True and args.pyunzip == None:
-        if args.process:
-            for i in range(int(sys.argv[3])):
-                newT = Process(target=pyzip('files'))
-                newT.start()
-                processes.append(newT)
-            for p in processes:
-                p.join()
-            print(args.pyunzip)
-            print(args.pyzip)
-        else:
-            pyzip('No Process')
-    
-    if args.pyunzip == True and args.pyzip == None:
-        if args.process:
-            for i in range(int(sys.argv[3])):
-                newT = Process(target=pyunzip('files'))
-                newT.start()
-                processes.append(newT)
-            for p in processes:
-                p.join()           
-        else:
-            pyunzip('No Process')
-    else:
-        parser.error("-c and -d can't be used with eachother.")
+	#argumentos a utilizar
+	parser = argparse.ArgumentParser(prog='pzip', description='Zip or unzip files')
+	parser.add_argument('-c', action='store_const', dest='pyzip', const=True)
+	parser.add_argument('-d', action='store_const', dest='pyunzip', const=True)
+	parser.add_argument('-p', metavar='n', nargs='?', action='append', dest='process', const=True)
+	parser.add_argument('-t', action='store_const', dest='targ', const=True)
+	parser.add_argument('{ficheiros}', nargs='+')    
+	args = parser.parse_args()
+
+	#lista dos processos
+	processes = []
+
+	#verificação de argumentos
+	if args.pyzip == True and args.pyunzip == None:
+
+		if args.process:
+			listArgs=[]
+			for i in sys.argv[4:]:
+				listArgs.append(i)
+			for i in range(int(sys.argv[3])):
+				newT = Process(target=pyzip(listArgs))
+				newT.start()
+				processes.append(newT)
+
+			for p in processes:
+				p.join()
+
+			
+		else:
+			listArgs = []
+			for i in sys.argv[2:]:
+				listArgs.append(i)
+			if len(listArgs) == 0:
+				
+			pyzip(listArgs)
+
+		print('Foram zippados',count.value,'ficheiros')
+	if args.pyunzip == True and args.pyzip == None:
+		if args.process:
+			listArgs=[]
+			for i in sys.argv[4:]:
+				listArgs.append(i)
+			for i in range(int(sys.argv[3])):
+				newT = Process(target=pyunzip(listArgs))
+				newT.start()
+				processes.append(newT)
+			for p in processes:
+				p.join()
+		else:
+			listArgs = []
+			for i in sys.argv[2:]:
+				listArgs.append(i)
+			pyunzip(listArgs)
+		print('Foram unzippados',count.value,'ficheiros')
+	if args.pyzip == True and args.pyunzip == True:
+		parser.error("-c and -d can't be used with eachother.")
+	
