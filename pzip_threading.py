54a37710c4b777ea28944c450c1b085de790cf92
Tjccs <tiago.jccs@gmail.com>
2017-11-22 13:36:26 +0000

    SO PROJETO

--- pzip_threading.py
+++ pzip_threading.py
@@ -1,63 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import os
 import sys
-from multiprocessing import Process, Value, Lock
-from threading import Semaphore, Thread
+from multiprocessing import Process, Value
+from threading import Semaphore, Thread, Lock
 import zipfile
 import argparse
 import time
 
 def pyzip(args, t=False):
         global count
-
+        
         if t == True:
                 try:
                         
                         while count.value < len(args):
-                                mutex.acquire()
                                 files = args[count.value]
                                 count.value += 1
-                                mutex.release()
                                 if os.path.isfile(files):
                                         with zipfile.ZipFile(files+'.zip', mode='w') as zf:
-                                                zf.write(files)
-                                  
+                                                
+                                                zf.write(files)                                     
                                 else:
-
-                                        raise IOError 
-                        
+                                     raise IOError 
                 except:
                         print 'File doesn\'t exist'
                         sys.exit()
-
         else:
-                
                 while count.value < len(args):
-                        mutex.acquire()
                         files = args[count.value]
                         count.value += 1
-                        mutex.release()
                         if os.path.isfile(files):
                                 
                                 with zipfile.ZipFile(files+'.zip', mode='w') as zf:
-                                        zf.write(files)   
-                
+                                        zf.write(files)
+                             
 def pyunzip(args, t=False):
-
         global count
         if t == True:
                 try:
                         while count.value < len(args):
-                                mutex.acquire()
                                 files = args[count.value]
                                 count.value += 1
-                                mutex.release()
                                 if os.path.isfile(files):
                                         with zipfile.ZipFile(files) as zf:
                                                 zf.extractall()
-                                                
+                                        
                                 else:
                                         raise IOError
                 except:
@@ -65,15 +53,13 @@ def pyunzip(args, t=False):
                         sys.exit()
         else:
                 while count.value < len(args):
-                        # zona critica
-                        mutex.acquire()
                         files = args[count.value]
                         count.value += 1
-                        mutex.release()
-                        #--------------
                         if os.path.isfile(files):
                                 with zipfile.ZipFile(files) as zf:
                                         zf.extractall()
+                                        
+
 
 if __name__ == '__main__':
         # argumentos a utilizar
@@ -84,11 +70,12 @@ if __name__ == '__main__':
         parser.add_argument('-t', action='store_const', dest='targ', const=True)
         parser.add_argument('{ficheiros}', nargs='*')    
         args = parser.parse_args()
-        count = Value('i', 0)
+        count = Value('i',0)
+        threadLock = Lock()
         start_time = time.time()
-        #lista dos processos
-        processes = []
-        mutex = Lock()
+        # lista dos threads
+        threads = []
+
         #verificação de argumentos
         if args.pyzip == True and args.pyunzip == None:
                 if args.process:
@@ -102,29 +89,28 @@ if __name__ == '__main__':
                                                 end = raw_input('Do you have more files(y, n)? ') 
                         else:
                                 listArgs = [elem for elem in sys.argv[4:]]
-
                                 if len(listArgs) == 0:
                                         end = 'y'
                                         while end != 'n':
                                                 i_files = raw_input('What file do you want to unzip? ')
                                                 listArgs.append(i_files)
                                                 end = raw_input('Do you have more files(y, n)? ') 
-
                         for i in range(int(sys.argv[3])):
                                 if args.targ:
                                         t = True
-                                        newT = Process(target=pyzip, args=(listArgs, t,))
+                                        newT = Thread(target=pyzip, args=(listArgs, t,))
                                         newT.start()
                                 else:
-                                        newT = Process(target=pyzip, args=(listArgs,))
+                                        newT = Thread(target=pyzip, args=(listArgs,))
                                         newT.start()
+                                
+                                threads.append(newT)
 
-                                processes.append(newT)
+                        for t in threads:
+                                t.join()
 
-                        for p in processes:
-                                p.join()
+			
                 else:
-                        
                         if args.targ:
                                 listArgs = [elem for elem in sys.argv[3:]]
                                 if len(listArgs) == 0:
@@ -150,8 +136,7 @@ if __name__ == '__main__':
                 print 'Tempo de execução: ',time.time() - start_time,'segundos'
         if args.pyunzip == True and args.pyzip == None:
                 if args.process:
-			
-                        if args.targ:
+	                if args.targ:
                                 listArgs = [elem for elem in sys.argv[5:]]
                                 if len(listArgs) == 0:
                                         end = 'y'
@@ -160,7 +145,7 @@ if __name__ == '__main__':
                                                 listArgs.append(i_files)
                                                 end = raw_input('Do you have more files(y, n)? ') 
                         else:
-                                listArgs=[elem for elem in sys.argv[4:]]
+                                listArgs = [elem for elem in sys.argv[4:]]
                                 if len(listArgs) == 0:
                                         end = 'y'
                                         while end != 'n':
@@ -170,16 +155,16 @@ if __name__ == '__main__':
                         for i in range(int(sys.argv[3])):
                                 if args.targ:
                                         t = True
-                                        newT = Process(target=pyunzip, args=(listArgs,t,))
-				        new.start()                                
-	                        else:
-				        newT = Process(target=pyunzip, args=(listArgs,))
-                                	newT.start()
+                                        newT = Thread(target=pyunzip, args=(listArgs, t,))
+                                        newT.start()
+                                else:
+                                        newT = Thread(target=pyunzip, args=(listArgs,))
+                                        newT.start()
                                 
-                                processes.append(newT)
+                                threads.append(newT)
 
-                        for p in processes:
-                                p.join()
+                        for t in threads:
+                                t.join()
                 else:
                         if args.targ:
                                 listArgs = [elem for elem in sys.argv[2:]]
@@ -206,8 +191,8 @@ if __name__ == '__main__':
                                         pyunzip(listArgs)
                                 else:
                                         pyunzip(listArgs)
-                
                 print 'Foram unzippados',count.value,'ficheiros'
                 print 'Tempo de execução: ',time.time() - start_time,'segundos'
         if args.pyzip == True and args.pyunzip == True:
                 parser.error("-c and -d can't be used with eachother.")
+	
